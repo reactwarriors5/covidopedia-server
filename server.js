f2236c128875b4fa16d5d9780d5ce78db9f6a93f
reactwarriors5 <reactwarriors@official@gmail.com>
2021-11-04 09:28:14 +0600

    some changes

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
-import express from 'express'
-import cors from 'cors'
-import { readdirSync } from 'fs'
-import mongoose from 'mongoose'
-import cookieParser from 'cookie-parser'
-const morgan = require('morgan')
-require('dotenv').config()
+import express from "express";
+import cors from "cors";
+import { readdirSync } from "fs";
+import mongoose from "mongoose";
+import cookieParser from "cookie-parser";
+const morgan = require("morgan");
+require("dotenv").config();
 
 // create express app
-const app = express()
+const app = express();
 
 // db
 mongoose
@@ -19,19 +19,19 @@ mongoose
       j: true,
     },
   })
-  .then(() => console.log('**DB CONNECTED**'))
-  .catch(err => console.log('DB CONNECTION ERR => ', err))
+  .then(() => console.log("**DB CONNECTED**"))
+  .catch((err) => console.log("DB CONNECTION ERR => ", err));
 
 // Middlewares
 const corsConfig = {
   origin: true,
   credentials: true,
-}
-app.use(cors(corsConfig))
-app.options('*', cors(corsConfig))
-app.use(express.json({ limit: '5mb' }))
-app.use(cookieParser())
-app.use(morgan('dev'))
+};
+app.use(cors(corsConfig));
+app.options("*", cors(corsConfig));
+app.use(express.json({ limit: "5mb" }));
+app.use(cookieParser());
+app.use(morgan("dev"));
 
 // app.use(function (req, res, next) {
 //   res.header('Access-Control-Allow-Origin', 'http://localhost:3000')
@@ -42,11 +42,10 @@ app.use(morgan('dev'))
 //   )
 //   next()
 // })
-
 // route
-readdirSync('./routes').map(r => app.use('/api', require(`./routes/${r}`)))
+readdirSync("./routes").map((r) => app.use("/api", require(`./routes/${r}`)));
 
 // port
-const port = process.env.PORT || 8000
+const port = process.env.PORT || 8000;
 
-app.listen(port, () => console.log(`Server is running on port ${port}`))
+app.listen(port, () => console.log(`Server is running on port ${port}`));
